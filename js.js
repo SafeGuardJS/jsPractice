96d3040f3819f1a7410390c3ec25fd4ca41ded99
AndreyBrz <ay.berez@gmail.com>
2019-01-04 15:58:07 +0200

    HM 3 Tast 1 -- Basic array

--- js.js
+++ js.js
@@ -2,35 +2,26 @@
 //HomeWork 2
 //Task 2
 
-var star = "*", nbsp = "&nbsp";
+var arr = new Array()
+var N = +prompt("Enter size of array", '');
 
-for (var i = 0; i < 5; i++) {
-    if ( i == 0 || i == 4){
-        for ( var j = 0; j < 20; j++) {
-            document.write(star);
-        }
-    }else{
-        for ( var j = 0; j < 20; j++) {
-            if ( j == 0 || j == 19){
-                document.write(star);
-            }else {
-                document.write(nbsp + nbsp);
-            }
-        }
-    }    
-    document.write("<br/>");
+arr.length = N;
+for (var i = 0; i < N; i++) {
+    arr[i] = Math.ceil(Math.random(100) * 100);
 }
 
-document.write("<hr>");
-for (var i = 0; i < 10; i++) {
-    for ( var j = 0; j < i; j++) {
-        document.write(star);
-    }    
-    document.write("<br/>");
+var Max = arr[0], Min = arr[0], Sum = 0, Middle = 0, arrEvenNumber = [];
+
+for (var i = 0; i < N; i++) {
+    if (Max < arr[i]) Max = arr[i];
+    if (Min > arr[i]) Min = arr[i];
+    if (arr[i] % 2 == 0) arrEvenNumber.push(arr[i]);
+    Sum += arr[i];
 }
+Middle = Sum / N;
 
-document.write("<hr>");
-
-
-
-
+document.write("<h1>" + "Initial Array<br/>" + "</h1>" + arr.join(" | ") + "<hr>");
+document.write("Minimal value :" + Min + "<br/>Maximum value :" + Max + "<hr>");
+document.write("Middel value of array :"+ Middle + "<hr>");
+document.write("Sum of array elements :"+ Sum + "<hr>");
+document.write("Whole even value :" + arrEvenNumber.join(" | ") + "<hr>");
